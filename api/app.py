170c8d50d5237620c7b436f8a51d8294e22c5f8b
kvncampos <kvnelpdfw@hotmail.com>
2024-03-21 18:23:48 -0500

    testeing post request

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -8,8 +8,15 @@ with open('docs/ctm_data.json', 'r') as file:
     ctm_data = json.load(file)
 
 # Route for /keys/all
-@app.route('/api/keys/all', methods=['GET'])
+@app.route('/api/keys/all', methods=['GET', 'POST'])
 def get_keys_data():
+    if request.method == 'POST':
+        # Handle the POST request
+        data = request.json  # Assuming JSON data is sent in the request
+        # Process the data as needed
+        # Return a JSON response with status code 200 to indicate success
+        return jsonify({"message": "Success"}), 200
+    
     return jsonify(ctm_data)
 
 if __name__ == '__main__':
